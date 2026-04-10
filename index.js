df02e766e2eb8ee193a0dff7a96fcdb152714101
Namrata.Rana@wissen.com <namrata.rana@wissen.com>
2026-04-11 02:03:45 +0530

    cors issue fixed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();                //this creates the express server which is
 // app.use(cors());                      //this tells our express server to allow cors enabling cross origin requests. Without it, the browser will block the requests.
 app.use(cors({
     // origin: 'http://localhost:5173',  // Replace with your frontend URL
-    origin : 'https://mock-api-simulator.vercel.app/',  // Replace with your frontend URL
+    origin : 'https://mock-api-simulator.vercel.app',  // Replace with your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowed HTTP methods
     allowedHeaders: ['Content-Type'],  // Allowed headers
 }));
